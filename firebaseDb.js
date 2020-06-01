b888834e457b32116529fe550e9bd00c9dad5615
NUSvigate <66207501+NUSvigate@users.noreply.github.com>
2020-06-01 11:15:51 +0800

    Add files via upload

--- firebaseDb.js
+++ firebaseDb.js
@@ -2,14 +2,14 @@ import * as firebase from 'firebase';
 import firestore from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBysrBLzV6ZEFcsO_k-D3CvbCBRhI4MaW0",
-    authDomain: "nusvigate.firebaseapp.com",
-    databaseURL: "https://nusvigate.firebaseio.com",
-    projectId: "nusvigate",
-    storageBucket: "nusvigate.appspot.com",
-    messagingSenderId: "794910415753",
-    appId: "1:794910415753:web:302c863a8c3fdfdd1d5f58",
-    measurementId: "G-2HHFNGE0F1"
+    apiKey: "AIzaSyB3PXIaXphYaiA-8ajiYUJ-BOPJBkqYZrc",
+    authDomain: "nusvigate-1fffe.firebaseapp.com",
+    databaseURL: "https://nusvigate-1fffe.firebaseio.com",
+    projectId: "nusvigate-1fffe",
+    storageBucket: "nusvigate-1fffe.appspot.com",
+    messagingSenderId: "797424671267",
+    appId: "1:797424671267:web:b4c62c26b915adcd4d2f96",
+    measurementId: "G-4Y6439RD6N"
 };
 
 firebase.initializeApp(firebaseConfig)
