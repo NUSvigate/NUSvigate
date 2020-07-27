fca5040f8ef2ebe952f72e6602bc21265c10f15c
NUSvigate <66207501+NUSvigate@users.noreply.github.com>
2020-07-27 15:43:14 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,25 +1,25 @@
-import React from "react";
-import { StyleSheet, SafeAreaView } from "react-native";
-import { createStore, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-import thunkMiddleware from "redux-thunk";
-import reducer from "./reducers";
-import AppNavigator from "./navigation/AppNavigator";
+import React from 'react';
+import { StyleSheet, SafeAreaView } from 'react-native';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import reducer from './reducers';
+import AppNavigator from './navigation/AppNavigator';
 
-const middleware = applyMiddleware(thunkMiddleware);
-const store = createStore(reducer, middleware);
+const middleware = applyMiddleware(thunkMiddleware)
+const store = createStore(reducer, middleware)
+console.disableYellowBox = true;
 
 export default function App() {
-  return (
-    <Provider store={store}>
-      <AppNavigator />
-    </Provider>
-  );
-  console.disableYellowBox = true;
+    return(
+        <Provider store={store}>
+            <AppNavigator />
+        </Provider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+    container: {
+        flex: 1,
+    }
 });
